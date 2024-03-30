a0e43042bea5af0ee234e0059cf557fd6bd0b535
rozzy <rosekaremeri@gmail.com>
2024-03-30 23:15:03 +0300

    Fix: withdrawing and price calculations

--- remin_fundme_contract/FundMe.sol
+++
@@ -1,4 +0,0 @@
-//This is a contract that will enable people to deposit funds into the contract
-//Enable the owner/ the one who deplys the contract to withdraw the funds
-//set the minimum amount of funds that one can deposit
-
---
+++ remix_fundme_contract/PriceConverter.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// Why is this a library and not abstract?
+// Why not an interface?
+library PriceConverter {
+    // We could make this public, but then we'd have to deploy it
+    function getPrice() internal view returns (uint256) {
+
+  //to get this; means we have to interract with a contract outside of our contract, hence need ABI and address
+            //we'll use chain link and since we are using the sepolia testnet, get the price of ETH/USD contract address - https://docs.chain.link/data-feeds/price-feeds/addresses/?network=ethereum&page=1
+            //0x694AA1769357215DE4FAC081bf1f309aDC325306
+            //to get ABI; we can import this directly from github at the top, like an npm package - import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+        // Sepolia ETH / USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses#Sepolia%20Testnet
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
+        // or (Both will do the same thing)
+        // return uint256(answer * 1e10); // 1* 10 ** 10 == 10000000000
+    }
+
+    // 1000000000
+    function getConversionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        // or (Both will do the same thing)
+        // uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1 * 10 ** 18 == 1000000000000000000
+        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+        return ethAmountInUsd;
+    }
+}
