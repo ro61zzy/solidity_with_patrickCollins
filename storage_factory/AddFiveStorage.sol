14de067224e66c51e744ba8b5a13b970d7a94ac9
rozzy <rosekaremeri@gmail.com>
2024-04-10 16:51:51 +0300

    contract iheritance basics

--- storage_factory/AddFiveStorage.sol
+++ storage_factory/AddFiveStorage.sol
@@ -1,2 +1,18 @@
+// SPDX-License-Identifier: MIT
+
 //inheritance
 //we want this contract to always add five to the favorite number stored in the simple storage contract.
+
+pragma solidity ^0.8.19;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+
+//we use the "is" to inherit the simple storage functions; we inherit functionalities
+contract AddFiveStorage is SimpleStorage {
+
+    //to add custom functionality,,
+    function store(uint256 _favoriteNumber) public override {
+        myFavoriteNumber = _favoriteNumber + 5;
+    }
+}
