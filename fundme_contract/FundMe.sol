b9a7ef19ead0699a651e52c1c5aa3e1be2d9dfca
rozzy <rosekaremeri@gmail.com>
2024-04-10 20:41:38 +0300

    set up fundme contract

---
+++ fundme_contract/FundMe.sol
@@ -0,0 +1,12 @@
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+//we want this contract to 
+// a.) get funds from users
+// b.) withdraw funds
+// c.) set the minimum funding value in usd, azin the minimum a person can send to the contract
+
+contract FundMe{
+    
+}
