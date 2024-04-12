7bb8172d7fbdfdc2999f776adec8894b1fcfa79a
rozzy <rosekaremeri@gmail.com>
2024-04-12 14:15:33 +0300

    funding a contract

--- fundme_contract/FundMe.sol
+++ fundme_contract/FundMe.sol
@@ -9,6 +9,7 @@ pragma solidity ^0.8.19;
 
 contract FundMe{
 
+    uint256 public minimumUsd = 5;
 
     //the two main functions that we need are, fund and withdraw
     function fund() public payable{
@@ -16,6 +17,11 @@ contract FundMe{
         // aslo set the minimum amount in dollar, a user can send
         //how do we send eth to a contract? to allow the function to receive, you make it payabele
         //so whenever this function is called, we will send eth to this contract
+        //require(msg.value > 1e18, "didn't send enough"); //we require users to send atleast 1 ether, revert to the message if it fails
+        //so if we want our users to send not less than $5 worth of eth, we need to get the price of eth and calculate and see how much we get
+
+        require(msg.value >= minimumUsd, "didn't send enough"); ///so we need to convert the usd to eth for msg.value to understand
+        //where oracles n chainlink come in; we need data from the real world
 
     }
 
