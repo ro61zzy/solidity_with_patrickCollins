278d3845ca56d947dedb4c4db4fda01fd68d7f3a
rozzy <rosekaremeri@gmail.com>
2024-04-12 15:57:39 +0300

    withdrawing funds, azin sending back to funder

--- fundme_contract/FundMe.sol
+++ fundme_contract/FundMe.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.19;
 
-import {PriceConverter} from "./PriceConverter";
+//import {PriceConverter} from "./PriceConverter";
 
 //we want this contract to 
 // a.) get funds from users
@@ -77,6 +77,38 @@ contract FundMe{
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+        //reset array to 0
+        funders = new address[](0);
 
+         // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \ 
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback() 
+    //     /   \ 
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
     }
 }
