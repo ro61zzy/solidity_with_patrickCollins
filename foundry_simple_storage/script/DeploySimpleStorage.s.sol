0c5efb18aa25383b7840023b75538f4d47bb816b
rozzy <rosekaremeri@gmail.com>
2024-07-01 22:14:01 +0300

    deploying to temporary local environment

--- foundry_simple_storage/script/DeploySimpleStorage.s.sol
+++ foundry_simple_storage/script/DeploySimpleStorage.s.sol
@@ -18,7 +18,15 @@ function run() external returns (SimpleStorage) {
 
 //The vm keyword is just a tool for controlling the interactions with Forge's local Ethereum testnet. We're using it here to specify that all the activities within the startBroadcast and stopBroadcast functions should take place on-chain.
 
-//to run this, forge script script/deploySimpleStorage.s.sol
+// //to run this, forge script script/deploySimpleStorage.s.sol
+// // Compiler run successful!
+// // Script ran successfully.
+// // Gas used: 338975
+
+// // == Return ==
+// // 0: contract SimpleStorage 0x90193C961A926261B756D1E5bb255e67ff9498A1
+
+// // If you wish to simulate on-chain transactions pass a RPC URL.
 
 
-//si
+//since we didn't specify any RPC URL; in foundry, it will automatically run/deploy your contract on a temporary environment
