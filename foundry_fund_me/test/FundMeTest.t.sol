bb1da22bcba214e209cd824cd457f59a0adf09f1
rozzy <rosekaremeri@gmail.com>
2024-07-02 12:31:32 +0300

    console logging in foundry

--- foundry_fund_me/test/FundMeTest.t.sol
+++ foundry_fund_me/test/FundMeTest.t.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.8;
 
 //naming convention of test file with .t.sol, best practice
+//forge test
+//to show logs forge test -vvv the v's are the number of logs
 //forge has built in commands that help testing easier, let's import these
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol"; //import console to log
 
 contract FundMETest is Test {
     //what we want to do is test our fund me contract is doing what is expected to do
     //first let's deploy our contract, we will do this pn script but for now let's just do this here
-    
+    function setUp() external{}
 }
