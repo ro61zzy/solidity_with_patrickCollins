f4c14c1dbbaaa2d82db4a339dae3c17b318134b5
rozzy <rosekaremeri@gmail.com>
2024-07-02 12:19:42 +0300

    naming errors with contract names

--- foundry_fund_me/src/FundMe.sol
+++ foundry_fund_me/src/FundMe.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
-
-
+error FundMe__NotOwner(); //it's best practice to name your error with contract name and two underscores
 
 
 contract FundMe {
@@ -15,7 +13,6 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
@@ -24,12 +21,9 @@ contract FundMe {
     }
 
     function fund() public payable {
-        //we want to be able to send the minimum amnt one can deposit
-        // i) how do we send ETH to this contract? make the function payable
-        //to get how much value one is sending, use msg.value;
-        //to set min
+
         require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+    
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
@@ -41,8 +35,8 @@ contract FundMe {
     }
     
     modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
+      
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
     
@@ -52,26 +46,11 @@ contract FundMe {
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-        // call
+       
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
+  
 
     fallback() external payable {
         fund();
