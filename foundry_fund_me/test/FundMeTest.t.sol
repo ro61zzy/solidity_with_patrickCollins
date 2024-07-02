93b3c75df86cdb2485bfa07d58f2b46fd844a8c3
rozzy <rosekaremeri@gmail.com>
2024-07-02 12:26:27 +0300

    smart contract testing i foundry

---
+++ foundry_fund_me/test/FundMeTest.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+//naming convention of test file with .t.sol, best practice
+//forge has built in commands that help testing easier, let's import these
+import {Test} from "forge-std/Test.sol";
+
+contract FundMETest is Test {
+    //what we want to do is test our fund me contract is doing what is expected to do
+    //first let's deploy our contract, we will do this pn script but for now let's just do this here
+    
+}
